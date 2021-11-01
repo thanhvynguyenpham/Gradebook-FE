5d7ab31b0200478d9f969fe778b5f4114b1a3f62
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2021-11-01 21:36:24 +0700

    remove unused import

--- src/Components/Class/index.jsx
+++ src/Components/Class/index.jsx
@@ -4,7 +4,6 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import "./class.scss";
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./Pages/Home";
 
