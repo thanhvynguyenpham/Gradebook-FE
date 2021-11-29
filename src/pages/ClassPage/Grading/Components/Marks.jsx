0a3427cdfe907340b0e6f42134ecd49efd67a80f
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2021-11-29 11:45:50 +0700

    send uploaded list to server

--- src/pages/ClassPage/Grading/Components/Marks.jsx
+++ src/pages/ClassPage/Grading/Components/Marks.jsx
@@ -1,94 +1,45 @@
-import { Button, Grid, Input } from "@mui/material";
-import React, { useState } from "react";
+import { Download, UploadFile } from "@mui/icons-material";
+import { Button, Grid, Paper, Stack, Typography } from "@mui/material";
+import React from "react";
 import { Link } from "react-router-dom";
-import XLSX from "xlsx";
-
-
-const convertToJson = (csv) =>{
-  var lines = csv.split("\n");
-
-  var result = [];
-
-  //var headers = lines[0].split(",");
-  const headers = ['studentId','fullName'];
-
-  for (var i = 1; i < lines.length&&lines[i]; i++) {
-    console.log("Line:",lines[i]);
-    var obj = {};
-    var currentline = lines[i].split(",");
-
-    for (var j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
-    }
-
-    result.push(obj);
-  }
-
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
-}
-
 
 const Marks = ({ hidden }) => {
-  const [students, setStudents] = useState([]);
-  const [cols, setCols] = useState([]);
-
-  const handleFile = (e) => {
-    const files = e.target.files;
-    if (!files || !files[0]) return;
-    const file = files[0];
-    const reader = new FileReader();
-    const rABS = !!reader.readAsBinaryString;
-    reader.onload = (e) => {
-      /* Parse data */
-      const bstr = e.target.result;
-      const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      console.log(rABS, wb);
-      /* Convert array of arrays */
-      //const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      /* Update state */
-      //setCols(data[0]);
-      //setStudents(data.slice(1));
-      console.log("Data>>>" + data);// shows that excel data is read
-      console.log(convertToJson(data));
-    };
-    if (rABS) reader.readAsBinaryString(file);
-    else reader.readAsArrayBuffer(file);
-  };
-  
   return (
     <Grid item xs={12} sm={10} hidden={hidden}>
-      <Grid container item xs={12} justifyContent="flex-end">
-        <Grid item xs={4}>
-          <Link
-            to="/assets/templates/student-list-template.xlsx"
-            target="_blank"
-            download
+      <Stack spacing={3} width={"100%"}>
+        <Paper elevation={3} className="top-panel">
+          <Grid
+            container
+            item
+            xs={12}
+            padding="20px"
+            spacing={2}
+            justifyContent="space-between"
           >
-            <Button size="small" variant="contained">
-              Student Template
-            </Button>
-          </Link>
-        </Grid>
-        <Grid item xs={4}>
-          <Link
-            to="/assets/templates/point-template.xlsx"
-            target="_blank"
-            download
-          >
-            <Button size="small" variant="outlined">
-              Point Template
-            </Button>
-          </Link>
-        </Grid>
-        <Grid item xs={12}>
-          <Input type="file" onChange={handleFile} />
-        </Grid>
-      </Grid>
+            <Grid item>
+              <Typography variant="h5">Class Mark</Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                size="small"
+                variant="contained"
+                startIcon={<UploadFile />}
+              >
+                Upload Marksheet
+              </Button>
+            </Grid>
+            {/* <Link
+              to="/assets/templates/point-template.xlsx"
+              target="_blank"
+              download
+            >
+              <Button size="small" variant="contained" startIcon={<Download />}>
+                Point Template
+              </Button>
+            </Link> */}
+          </Grid>
+        </Paper>
+      </Stack>
     </Grid>
   );
 };
--- src/pages/ClassPage/Grading/Components/StudentList.jsx
+++ src/pages/ClassPage/Grading/Components/StudentList.jsx
@@ -1,12 +1,34 @@
-import { Button, Grid, Input } from "@mui/material";
+import { Download } from "@mui/icons-material";
+import {
+  Button,
+  Grid,
+  Input,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import XLSX from "xlsx";
+import { convertToJson } from "../../../../Utils/converters";
+import { postAuth } from "../../../../Utils/httpHelpers";
 
-const StudentList = ({ hidden }) => {
-  const [students, setStudents] = useState([]);
-  const [cols, setCols] = useState([]);
-
+const StudentList = ({
+  hidden,
+  students,
+  setStudents,
+  classDetails,
+  setAlertMessage,
+  setOpenAlertMessage,
+}) => {
+  const [studentLists, setStudentLists] = useState(students);
+  const [btnDisabled, setBtnDisabled] = useState(false);
   const handleFile = (e) => {
     const files = e.target.files;
     if (!files || !files[0]) return;
@@ -16,44 +38,119 @@ const StudentList = ({ hidden }) => {
     reader.onload = (e) => {
       /* Parse data */
       const bstr = e.target.result;
-      const wb = XLSX.read(bstr, { type: "binary" });
+      const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
       /* Get first worksheet */
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
       console.log(rABS, wb);
       /* Convert array of arrays */
-      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
       /* Update state */
-      setCols(data[0]);
-      console.log(data[0]);
-      const body = {
-        studentList: JSON.stringify(data.slice(1)),
-      };
-      console.log(body);
-      setStudents(data.slice(1));
+      setStudentLists(convertToJson(data));
+      console.log(convertToJson(data));
     };
     if (rABS) reader.readAsBinaryString(file);
     else reader.readAsArrayBuffer(file);
   };
-
+  const onUpdateStudents = () => {
+    setBtnDisabled(true);
+    const body = {
+      listStudents: JSON.stringify(studentLists),
+    };
+    postAuth(`/class/${classDetails.id}/list-students`, body)
+      .then((response) => {
+        setBtnDisabled(false);
+        setStudents(response.data);
+        setAlertMessage("Update student list successfully.");
+        setOpenAlertMessage(true);
+      })
+      .catch((error) => {
+        setBtnDisabled(false);
+        setAlertMessage(error.response.error);
+        setOpenAlertMessage(true);
+      });
+  };
   return (
     <Grid item xs={12} sm={10} hidden={hidden}>
-      <Grid container item xs={12} justifyContent="flex-end">
-        <Grid item xs={4}>
-          <Link
-            to="/assets/templates/student-list-template.xlsx"
-            target="_blank"
-            download
+      <Stack spacing={3} width={"100%"}>
+        <Paper elevation={3} className="top-panel">
+          <Grid
+            container
+            item
+            xs={12}
+            justifyContent="space-between"
+            padding="20px"
           >
-            <Button size="small" variant="contained">
-              Student Template
-            </Button>
-          </Link>
-        </Grid>
-        <Grid item xs={12}>
-          <Input type="file" onChange={handleFile} />
-        </Grid>
-      </Grid>
+            <Grid item>
+              <Input
+                type="file"
+                onChange={handleFile}
+                style={{ color: "white" }}
+              />
+            </Grid>
+            <Grid item>
+              <Link
+                to="/assets/templates/student-list-template.xlsx"
+                target="_blank"
+                download
+              >
+                <Button
+                  size="small"
+                  variant="contained"
+                  startIcon={<Download />}
+                >
+                  Student Template
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper elevation={2} style={{ padding: "20px" }}>
+          <Typography variant="h5">Preview</Typography>
+          {studentLists.length !== 0 ? (
+            <>
+              <TableContainer>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Student ID</TableCell>
+                      <TableCell>Student Name</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {studentLists.map((student, index) => (
+                      <TableRow
+                        key={index}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {student.studentId}
+                        </TableCell>
+                        <TableCell>{student.fullName}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <Grid container xs={12} justifyContent="center" padding="10px">
+                <Button
+                  variant="outlined"
+                  onClick={onUpdateStudents}
+                  disabled={btnDisabled}
+                >
+                  Update Students List
+                </Button>
+              </Grid>
+            </>
+          ) : (
+            <Typography variant="body1" textAlign="center">
+              No file chosen
+            </Typography>
+          )}
+        </Paper>
+      </Stack>
     </Grid>
   );
 };
