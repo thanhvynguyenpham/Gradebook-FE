25daeb7d025462e01cb2a8d6d427aa8ea62b6d37
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2021-12-21 10:57:10 +0700

    build join class UI

---
+++ src/Components/Form/JoinClassForm.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { postAuth } from "../../Utils/httpHelpers";
+
+const validationSchema = yup.object({
+  code: yup.string("Enter a class code").required("Class code is required"),
+});
+
+const JoinClassForm = ({ isShow, handleClose, onJoinFailed }) => {
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      code: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      setBtnDisabled(true);
+      submitForm(values);
+      formik.resetForm();
+    },
+  });
+
+  useEffect(() => {
+    setBtnDisabled(false);
+  }, [isShow]);
+
+  function submitForm(values) {
+    const body = {
+      code: values.classcode,
+    };
+    postAuth("/classes", body)
+      .then((response) => {
+        // move to classpage
+      })
+      .catch((error) => {
+        console.log(error);
+        onJoinFailed("Cannot join class");
+        setBtnDisabled(false);
+      });
+  }
+
+  function onClose() {
+    handleClose();
+    formik.resetForm();
+  }
+
+  return (
+    <div>
+      <Dialog open={isShow} onClose={handleClose}>
+        <form onSubmit={formik.handleSubmit}>
+          <DialogTitle>Join A Class</DialogTitle>
+          <DialogContent>
+            <TextField
+              disabled={btnDisabled}
+              autoFocus
+              margin="dense"
+              id="code"
+              label="Class Code"
+              type="text"
+              fullWidth
+              variant="standard"
+              value={formik.values.code}
+              onChange={formik.handleChange}
+              error={formik.touched.code && Boolean(formik.errors.code)}
+              helperText={formik.touched.code && formik.errors.code}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onClose} disabled={btnDisabled}>
+              Cancel
+            </Button>
+            <Button type="submit" disabled={btnDisabled}>
+              Join class
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
+
+export default JoinClassForm;
