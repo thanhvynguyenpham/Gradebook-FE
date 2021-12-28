f4504b2c73a367aca7895daf7da4627de395e3fc
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2021-12-28 11:27:34 +0700

    grade list of student

---
+++ src/pages/ClassPage/StudentGrading/Components/CreateRequest.jsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Slide,
+  Grid,
+} from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+export default function CreateRequest({ open, handleClose, assignments }) {
+  const [asignment, setAsignment] = React.useState("");
+
+  const handleChange = (event) => {
+    setAsignment(event.target.value);
+  };
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+        keepMounted
+      >
+        <DialogTitle>Request A Review</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <DialogContentText>
+                Send your expected point and reason for reviewing to your
+                teachers.
+              </DialogContentText>
+            </Grid>
+            <Grid item xs={4}>
+              <FormControl fullWidth required>
+                <InputLabel id="demo-simple-select-label">
+                  Assignment
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Assignment"
+                  onChange={handleChange}
+                >
+                  {assignments &&
+                    assignments.grades.map((item) => (
+                      <MenuItem key={item.identity} value={item.identity}>
+                        {item.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                required
+                autoFocus
+                id="name"
+                label="Expected Point"
+                type="number"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField required multiline fullWidth label="Reason" />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Send Request</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/pages/ClassPage/StudentGrading/index.jsx
+++ src/pages/ClassPage/StudentGrading/index.jsx
@@ -2,43 +2,160 @@ import {
   Container,
   Grid,
   Snackbar,
-  Tabs,
-  Tab,
-  useMediaQuery,
+  Button,
+  Paper,
+  Stack,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Skeleton,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { getAuth } from "../../../Utils/httpHelpers";
 import "../Grading/index.scss";
-import StudentPoint from "./Components/StudentPoint";
-import ManageReview from "./Components/ManageReview";
+import CreateRequest from "./Components/CreateRequest";
 function StudentGrading({ hidden, classDetails }) {
-  const [value, setValue] = useState(0);
   const [openAlertMessage, setOpenAlertMessage] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
-  const smDown = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const [assignments, setAssignments] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [openForm, setOpenForm] = useState(false);
+  useEffect(() => {
+    const fetchData = () => {
+      setIsLoading(true);
+      getAuth(`/class/${classDetails._id}/student/grades-board`)
+        .then((response) => {
+          setAssignments(response.data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsLoading(false);
+        });
+    };
+    fetchData();
+  }, [classDetails]);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const openCreateRequestDialog = () => {
+    setOpenForm(true);
+  };
+  const closeCreateRequestDialog = () => {
+    setOpenForm(false);
   };
   return (
     <div hidden={hidden}>
       <Container sx={{ mt: 4, mb: 2 }}>
         <Grid container spacing={2} xs={12} justifyContent="center">
-          <Grid item sm={2}>
-            <Tabs
-              orientation={smDown ? "horizontal" : "vertical"}
-              aria-label="Vertical tabs"
-              sx={{ borderRight: 1, borderColor: "divider" }}
-              value={value}
-              onChange={handleChange}
-            >
-              <Tab label="Marks" key="tab-1" />
-              <Tab label="Reviews" key="tab-2" />
-            </Tabs>
+          <Grid item xs={12} sm={10} hidden={hidden}>
+            <Stack spacing={3} width={"100%"}>
+              <Paper elevation={3} className="top-panel">
+                <Grid
+                  container
+                  item
+                  xs={12}
+                  padding="20px"
+                  spacing={2}
+                  justifyContent="space-between"
+                >
+                  <Grid item>
+                    <Typography variant="h5">Class Mark</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      onClick={openCreateRequestDialog}
+                    >
+                      Request a review
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Paper>
+              {isLoading ? (
+                <Paper elevation={2} style={{ padding: "20px" }}>
+                  <Skeleton animation="wave" />
+                  <Skeleton animation="wave" />
+                </Paper>
+              ) : (
+                <Paper elevation={2} style={{ padding: "20px" }}>
+                  {assignments.grades && assignments.grades.length !== 0 ? (
+                    <TableContainer>
+                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell></TableCell>
+                            {assignments &&
+                              assignments.grades.map((assignment) => (
+                                <TableCell>{assignment.name}</TableCell>
+                              ))}
+                            <TableCell>Total</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          <TableRow
+                            sx={{
+                              "&:last-child td, &:last-child th": { border: 0 },
+                            }}
+                          >
+                            <TableCell component="th" scope="row">
+                              Grade Structure
+                            </TableCell>
+                            {assignments &&
+                              assignments.grades.map((assignment, i) => (
+                                <TableCell key={`grade-structure-${i}`}>
+                                  {assignment.pointStructure}
+                                </TableCell>
+                              ))}
+                            <TableCell
+                              component="th"
+                              scope="row"
+                              key={`grade-structure-total`}
+                            >
+                              10
+                            </TableCell>
+                          </TableRow>
+                          <TableRow
+                            sx={{
+                              "&:last-child td, &:last-child th": { border: 0 },
+                            }}
+                          >
+                            <TableCell component="th" scope="row">
+                              Score
+                            </TableCell>
+                            {assignments &&
+                              assignments.grades.map((assignment, i) => (
+                                <TableCell key={`point-${i}`}>
+                                  {assignment.point ?? ""}
+                                </TableCell>
+                              ))}
+                            <TableCell component="th" scope="row" key={`total`}>
+                              {assignments && assignments.total
+                                ? assignments.total
+                                : ""}
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  ) : (
+                    <Typography variant="body1" textAlign="center">
+                      No final grade yet.
+                    </Typography>
+                  )}
+                </Paper>
+              )}
+            </Stack>
           </Grid>
-          <StudentPoint hidden={value !== 0} />
-          <ManageReview hidden={value !== 1} />
         </Grid>
       </Container>
+      <CreateRequest
+        open={openForm}
+        handleClose={closeCreateRequestDialog}
+        assignments={assignments}
+      />
       <Snackbar
         open={openAlertMessage}
         autoHideDuration={4000}
